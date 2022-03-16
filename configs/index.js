6231abb62e548bc444f0d5f79333d2dbdc891fc4
imran12357 <imrankhanansari12357@gmail.com>
2022-03-16 11:59:33 +0530

    new changes

--- configs/index.js
+++ configs/index.js
@@ -1,5 +1,5 @@
 const config ={
-    dbconstr :'mongodb+srv://imran12357:password@cluster0.ze5ov.mongodb.net/api'
+    dbconstr :'mongodb+srv://imran12357:8801965509@cluster0.ze5ov.mongodb.net/test'
 }
 
 module.exports =config
